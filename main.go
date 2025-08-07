cc15da42044f443ac953ed814a0b013b86841c05
Erfi Anugrah <erfi.anugrah@gmail.com>
2025-08-07 11:36:31 +0200

    REPL with command structs

--- main.go
+++ main.go
@@ -1,15 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		output := cleanInput(input)
+
+		if len(output) > 0 {
+			commandName := output[0]
+			if command, exists := commands[commandName]; exists {
+				err := command.callback()
+				if err != nil {
+					fmt.Print(err)
+				}
+			} else {
+				fmt.Print("Unknown command \n")
+			}
+		}
+	}
 }
 
 func cleanInput(text string) []string {
+
 	formattedText := strings.Fields(strings.ToLower(text))
 	return formattedText
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex...Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\n Usage: \n help: Displays a help message \n exit: Exit the Pokedex \n")
+	return nil
 }
